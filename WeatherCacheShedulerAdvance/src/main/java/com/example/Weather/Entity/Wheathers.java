6abc00b0e037dfdd84ba6b4d22c3aa1f21c494e8
Abhishek-soft-hub <abhipersonal4444@gmail.com>
2025-12-11 17:29:26 +0530

    Id

--- WeatherCacheShedulerAdvance/src/main/java/com/example/Weather/Entity/Wheathers.java
+++ WeatherCacheShedulerAdvance/src/main/java/com/example/Weather/Entity/Wheathers.java
@@ -1,11 +1,14 @@
 package com.example.Weather.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Wheathers {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int cityId;
 	private int temp;
 	private String cityName;
